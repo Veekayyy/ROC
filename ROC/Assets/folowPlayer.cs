7840056d2c6315af68142901789a88a009424867
Veekayyy <54856385+Veekayyy@users.noreply.github.com>
2019-10-10 14:55:09 -0400

    camera folow + zoom -cinemachine

---
+++ ROC/Assets/folowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class folowPlayer : MonoBehaviour
+{
+
+   private Vector3 posCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+      posCamera = GameObject.Find("Hero").GetComponent<Transform>().position;
+      posCamera.z = -5;
+      GetComponent<Transform>().position = posCamera;
+    }
+}
---
+++ ROC/Assets/zoom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoom : MonoBehaviour
+{
+    
+    void Update()
+    {
+      if (Input.GetAxis ("Mouse ScrollWheel") > 0 && GetComponent<Camera>().orthographicSize > 2)
+      {
+         GetComponent<Camera>().orthographicSize -= 1;
+      }
+
+      if (Input.GetAxis("Mouse ScrollWheel") < 0 && GetComponent<Camera>().orthographicSize < 10)
+      {
+         GetComponent<Camera>().orthographicSize += 1;
+      }
+      
+    }
+}
