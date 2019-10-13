25aaa7f846a3ff8496af1b27a7a5a017f114e277
veekayyy <1655932@cstj.qc.ca>
2019-10-13 19:26:41 -0400

    Commit Vincent Attaque melee + distance v.0.1

---
+++ ROC/Assets/OnHit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHit : MonoBehaviour
+{
+
+    public GameObject effet;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effetG = Instantiate(effet, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        Destroy(effetG, 1f);
+
+
+
+    }
+}
