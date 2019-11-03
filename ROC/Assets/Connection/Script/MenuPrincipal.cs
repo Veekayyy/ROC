df9692cab0762e750a95de650b91343c8475148f
gabrieldgodon <gabrieldgodon.cssmi@gmail.com>
2019-11-03 15:04:19 -0500

    Menu de console de jeu vidéo

---
+++ ROC/Assets/Connection/Script/MenuPrincipal.cs
@@ -0,0 +1,39 @@
+﻿// But : Programmer les options avancées du menu.
+// Auteur : Gabriel Duquette Godon
+// Date : 27 octobre 2019
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+   #region Attribut
+   [SerializeField] Button btnFermuture;
+   [SerializeField] Animator canvasAnimation;
+   #endregion
+
+   #region Méthode publique
+   
+   public void entrerDansMenuPrincipal()
+   {
+      // On actionne l’animation pour faire apparaitre la fenêtre du menu principal.
+      canvasAnimation.SetTrigger("MenuJeu");
+   }
+
+   /// <summary>
+   /// Permettre à l’utilisateur de fermer l’application par l’option.
+   /// </summary>
+   public void fermetureApplication()
+   {
+      Application.Quit();   
+   }
+
+   public void jouer()
+   {
+      SceneManager.LoadScene("Main", LoadSceneMode.Single);
+   }
+   #endregion
+}
